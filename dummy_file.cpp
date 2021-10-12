cc2fd1b9ea9a5994fddafc19b5a462055beaf9b6
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2021-10-12 15:04:05 -0400

    build: add dummy file for compilation on mac

---
+++ dummy_file.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "graph_zeppelin_common.h"
+
+namespace GROUP_NAME {
+namespace EasterEgg {
+
+class EasterEgg {
+public:
+  void output() {
+    std::cout << "Here's an easter egg!" << std::endl;
+  }
+};
+
+} // namespace EasterEgg
+} // namespace GROUP_NAME
