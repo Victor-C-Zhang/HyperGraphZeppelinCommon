73135609575aa4dac01299cd56a6aeebda1f579e
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2021-11-09 16:43:03 -0500

    feat: declare node_id_t

--- graph_zeppelin_common.h
+++ graph_zeppelin_common.h
@@ -3,4 +3,4 @@
 #define GROUP_NAME GraphZeppelin
 #define BUFFERING_NAME GutterTree
 
-typedef uint64_t node_t; //Max graph vertices is 6 074 001 000
+typedef uint32_t node_id_t; // Max graph vertices is 2^32 - 1 = 4 294 967 295
