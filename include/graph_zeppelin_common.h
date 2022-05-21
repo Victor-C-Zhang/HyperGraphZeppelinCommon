01bd3886591bc00c8e82b73a3a9fe033d3a032a9
Victor Zhang <xskt.victor@gmail.com>
2022-05-21 17:15:15 -0400

    fix: vec_t to be 128 bit

--- include/graph_zeppelin_common.h
+++ include/graph_zeppelin_common.h
@@ -5,10 +5,21 @@
 
 #include <cstdint>
 
+
+#ifdef __SIZEOF_INT128__
+typedef __int128 int128_t;
+typedef unsigned __int128 uint128_t;
+#else
+typedef struct {
+  uint64_t low64;
+  uint64_t high64;
+} uint128_t;
+#endif
+
 // graph
 typedef uint32_t node_id_t; // Max graph vertices is 2^32 - 1 = 4 294 967 295
 typedef uint64_t edge_id_t; // Max number edges
 
 // sketching
-typedef uint64_t vec_t; //Max sketch vector size is 2^64 - 1
+typedef uint128_t vec_t; //Max sketch vector size is 2^64 - 1
 typedef uint32_t vec_hash_t;
